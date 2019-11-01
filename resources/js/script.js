67c88a26d3fa93bc2e9095f4a3cbcfc028b86376
Ipsita Rao <ipsita.rao@gmail.com>
2019-11-01 11:53:19 -0400

    i think im done for now

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("btn").innerHTML ="ipsita.rao@gmail.com";
-  }
+}
+
+let arrowBtn = document.querySelector(".arrowButton")
+
+arrowBtn.addEventListener("click", function(){
+    document.getElementById('howtosec').scrollIntoView();
+})
